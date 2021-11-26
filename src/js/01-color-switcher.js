83e178a72c2e6adfc2706f75baefdaf47c392a90
DyatlovSergey <s.dyatlov90@gmail.com>
2021-11-26 23:22:46 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,11 +1,39 @@
-const refs = {
-startBtn: document.querySelector('[data-start]'),
-stopBtn: document.querySelector('[data-stop]'),
+/*
+ * Метод setInterval(callback, delay, args)
+ */
 
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  timerId: null,
+};
+
+function changeColor() {
+  document.body.style.background = `${getRandomHexColor()}`;
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+function onClickStartBtn() {
+  changeColor();
+  refs.startBtn.setAttribute('disabled', true);
+  refs.stopBtn.removeAttribute('disabled');
+  refs.timerId = setInterval(() => {
+    changeColor();
+  }, 1000);
+}
 
+/*
+ * Очистка интервала с clearInterval(intervalId)
+ */
 
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
+function onClickStopBtn() {
+  clearInterval(refs.timerId);
+  refs.startBtn.removeAttribute('disabled');
+  refs.stopBtn.setAttribute('disabled', true);
+}
+
+refs.startBtn.addEventListener('click', onClickStartBtn);
+refs.stopBtn.addEventListener('click', onClickStopBtn);
+refs.stopBtn.setAttribute('disabled', true);
