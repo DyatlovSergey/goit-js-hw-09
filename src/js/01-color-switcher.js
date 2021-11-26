46f58a8ef6f611778e0c67c6f57bf1b30835bc09
DyatlovSergey <s.dyatlov90@gmail.com>
2021-11-26 17:16:30 +0200

    start files

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,11 @@
+const refs = {
+startBtn: document.querySelector('[data-start]'),
+stopBtn: document.querySelector('[data-stop]'),
 
+}
+
+
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
